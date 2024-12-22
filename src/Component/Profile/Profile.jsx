da378fd3c0e8f6727f9d4085f5e8166a9877142e
hadeer <hadeer.abelgawad44@gmail.com>
2024-12-22 22:12:30 +0200

    updated proj

--- src/Component/Profile/Profile.jsx
+++ src/Component/Profile/Profile.jsx
@@ -1,8 +1,12 @@
 function Profile({userData}) {
   return (
+    
     <div>
-     Profile
+        <div dir="ltr">
+          <h1 style={{fontWeight:'400', fontSize:'40px', lineHeight:'56px'}}>ملف المؤسسه</h1>
+        </div>
     </div>
+    
   );
 }
 
